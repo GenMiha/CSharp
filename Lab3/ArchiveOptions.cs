a888d950bf8c118cc0bdbfb5e51581ce52ba43f4
Mihail Bogomolov <michail.bogomolov@gmail.com>
2021-01-11 21:20:14 +0300

    Add project files.

---
+++ Lab3/ArchiveOptions.cs
@@ -0,0 +1,13 @@
+﻿using System.IO.Compression;
+
+namespace Lab3
+{
+    public class ArchiveOptions
+    {
+        public ArchiveOptions() { }
+
+        public bool NeedToArchive { get; set; }
+
+        public CompressionLevel Level { get; set; }
+    }
+}
---
+++ ServiceLibrary/ConfigManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ServiceLibrary
+{
+    public class ConfigManager : IParser
+    {
+        IParser parser;
+
+        public ConfigManager(string path, Type mainType)
+        {
+            if (path.EndsWith(".xml"))
+            {
+                parser = new XmlParser(path, mainType);
+            }
+            else if (path.EndsWith(".json"))
+            {
+                parser = new JsonParser(path, mainType);
+            }
+            else
+            {
+                throw new ArgumentNullException($"invalid extension");
+            }
+        }
+
+        public T GetOptions<T>() => parser.GetOptions<T>();
+    }
+}
